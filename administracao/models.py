bfbce71401083ffc925fa0e6ff88dc880e52ee22
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-11-17 22:35:29 -0300

    creating models

--- administracao/models.py
+++ administracao/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.fields import NullBooleanField
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+
+class Servico(models.Model):
+    ICONE_CHOICES = (
+        ('twf-cleaning-1', 'twf-cleaning-1'),
+        ('twf-cleaning-2', 'twf-cleaning-2'),
+        ('twf-cleaning-3', 'twf-cleaning-3'),
+    )
+
+    nome = models.CharField(max_length=50, null=False, blank=False)
+    valor_minimo = models.IntegerField(null=False, blank=False)
+    qtd_horas = models.IntegerField(null=False, blank=False)
+    porcentagem_comissao = models.FloatField(null=False, blank=False)
+    horas_quarto = models.IntegerField(null=False, blank=False)
+    valor_quarto = models.FloatField(null=False, blank=False)
+    horas_sala = models.IntegerField(null=False, blank=False)
+    valor_sala = models.FloatField(null=False, blank=False)
+    horas_banheiro = models.IntegerField(null=False, blank=False)
+    valor_banheiro = models.FloatField(null=False, blank=False)
+    horas_cozinha = models.IntegerField(null=False, blank=False)
+    valor_cozinha = models.FloatField(null=False, blank=False)
+    horas_quintal = models.IntegerField(null=False, blank=False)
+    valor_quintal = models.FloatField(null=False, blank=False)
+    horas_outros = models.IntegerField(null=False, blank=False)
+    valor_outros = models.FloatField(null=False, blank=False)
+    icone = models.CharField(null=False, blank=False, choices=ICONE_CHOICES, max_length=14) #opcoes de icones
+    posicao = models.IntegerField(null=False) #definindo a posicao na lista de diarias    
+    
