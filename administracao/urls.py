f864acffd4978cbecf590e92c4236a9798a12774
renata monteiro <r.cardoso.monteiro1@gmail.com>
2021-11-23 14:30:25 -0300

    user list completed

--- administracao/urls.py
+++ administracao/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('servicos/listar', servico_views.listar_servicos, name='listar_servico'),
     path('servicos/editar/<int:id>', servico_views.editar_servico, name='editar_servico'),
     path('servicos/remover/<int:id>', servico_views.remover_servico, name='remover_servico'),
-    path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name='cadastrar_usuario')
+    path('usuarios/cadastrar', usuario_views.cadastrar_usuario, name='cadastrar_usuario'),
+    path('usuarios/listar', usuario_views.listar_usuarios, name='listar_usuarios'),
 ]
--- administracao/views/usuario_views.py
+++ administracao/views/usuario_views.py
@@ -1,12 +1,20 @@
 from ..forms.usuario_forms import UsuarioForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
 
 #usaremos o sistema de autenticação do proprio django
 def cadastrar_usuario(request):
+    if request.method == 'POST':
     #formulario já aumtomatico do Django para cadastro de usuários // nao transforma em super usário
-    form_usuario = UsuarioForm(request.POST)
-    if form_usuario.is_valid():
-        form_usuario.save()
+        form_usuario = UsuarioForm(request.POST)
+        if form_usuario.is_valid():        
+            form_usuario.save()
+            return redirect('listar_usuarios')
     else: #se o metodo não for POST saberemos que temos que renderizar um form vazio
         form_usuario = UsuarioForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+def listar_usuarios(request):
+    User = get_user_model() #pegando o model de autenticacao do proprio django para renderizar e listar
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
